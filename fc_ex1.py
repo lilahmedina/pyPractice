6ef19664a4a9d82a9eae57c0a38e8d4aad0ae32c
Medina <delilah.medina@kla.com>
2025-05-10 10:07:13 -0700

    more practice

---
+++ fc_ex1.py
@@ -0,0 +1,8 @@
+# Delilah Medina
+# Write a program that uses input to prompt a user for their name 
+# and welcome them. 
+
+
+name = input('What is your name? ')
+print(f'Welcome ' + name + ' you look beautiful today!' )
+
---
+++ hw_7.py
@@ -0,0 +1,7 @@
+ # delilah medina 
+
+x = int(input("Enter a 1st number : "))
+y = int(input("Enter a 2nd number : "))
+
+print( x % x != 0 or y % y != 0)
+
